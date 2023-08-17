1f655ea5c90038cc3d21255adf16c317cbf638e5
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-08-17 17:25:08 -0400

    addition of login python

--- server/app.py
+++ server/app.py
@@ -8,5 +8,19 @@ class Welcome(Resource):
         return 'Welcome To The Party'
 api.add_resource(Welcome, '/')
 
+class Login(Resource):
+    def post(self):
+        data= request.get_json()
+        username = data['username']
+        password = data['password']
+
+        user = User.query.filter(User.username == username).first()
+        session['user_id'] = user.id
+        if user and user.authenticate(password):
+            return user.to_dict(), 200
+        return make_response({'error': '401 Unauthroized'}, 401)
+
+api.add_resource(Login, '/login', endpoint = 'login')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
