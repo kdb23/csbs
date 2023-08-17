30e8bbd8df997bf2e978b89c68519b823b606257
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-08-17 17:16:03 -0400

    addition of .env and a config file beginnings

---
+++ server/config.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_cors import CORS
+from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///models.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+md = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
