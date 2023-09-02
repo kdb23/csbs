a71faaeb8a45e0fb3cd9345eba058c633537b917
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-09-01 21:20:44 -0400

    signup componenet complete

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -5,17 +5,53 @@ import { useHistory } from 'react-router-dom'
 function SignUp() {
     
     const {setUser} = useContext(UserContext);
-
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
     const history = useHistory();
+
+    function handleSignup(e) {
+        e.preventDefault();
+        fetch("/users/list")
+            .then(response => response.json())
+            .then(users => {
+                console.log(username)
+                const usernames = users.map(user => user.username)
+                if (usernames.includes(username)) {
+                    window.alert("howdy")
+                } else if (password.length < 1) {
+                    wiindown.alert("hoedy")
+                }
+                fetch("/users", {
+                    method: "POST",
+                    headers: { "Content-Type" : "application/json"},
+                    body: JSON.stringify({
+                        username,
+                        password
+                    }),
+                })
+                .then((response) => response.json())
+                .then((data) => {
+                    setUser(data)
+                })
+                .catch((error) => {
+                    setError(error.message)
+                })
+            }) 
+            .catch((error) => {
+                setError(error.message)
+            })
+    }
 
     return(
         <div>
-            <form>
+            <form onSubmit={handleSignup}>
                 <label htmlFor='username'>Username:</label>
                 <input
                     type='username'
                     id='username'
                     autoComplete='off'
+                    onChange={(e) => setUsername(e.target.value)}
                 />
                 <div>
                     <label htmlFor='password'>Password:</label>
@@ -23,6 +59,7 @@ function SignUp() {
                         type='password'
                         id='password'
                         autoComplete='off'
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
                 <button>Submit</button>
