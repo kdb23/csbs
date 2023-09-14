4ff1caeb5a1bf1e7c5a0fc8769c420eb0d07b187
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-09-14 16:02:46 -0400

    addition of image file

--- client/src/Photo.js
+++ client/src/Photo.js
@@ -4,6 +4,7 @@ function Photo() {
     return(
         <div>
             <h1>Test Test</h1>
+            <img src="/images/noimage.jpeg" alt='User Phot Not Found' className='img-fluid rounded-border' style={{width: '50%', maxHeight: '300px'}} />
         </div>
     )
 }
