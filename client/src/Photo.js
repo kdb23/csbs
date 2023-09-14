1c15d2360a305299c7d37e876a63a4b4cb7f0f12
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-09-14 16:08:24 -0400

    changes to styling

--- client/src/Photo.js
+++ client/src/Photo.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 function Photo() {
     return(
-        <div>
-            <h1>Test Test</h1>
             <img src="/images/noimage.jpeg" alt='User Phot Not Found' className='img-fluid rounded-border' style={{width: '30%', maxHeight: '375px'}} />
-        </div>
     )
 }
 
