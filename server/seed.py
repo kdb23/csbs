c1ef17330d49f1aee1d2c3a53d365e3515f8783c
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-09-16 20:41:37 -0400

    changes to seed file for member/prayer instance

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,7 @@
 from faker import Faker
 from random import choice as rc
 from config import app
-from models import db, Prayer, Member
+from models import db, Prayer, Member, MPInstance
 import random
 
 fake = Faker()
@@ -27,20 +27,47 @@ def make_prayers():
     members = Member.query.all()
     prayers = []
 
-    description = ["Unspoken", "Family Support", "Martial Issues", "Financial Concern", "Health Concern" ]
-    
+    description = ["Unspoken", "Family Support", "Martial Issues", "Financial Concern", "Health Concern"]
+
     for member in members:
-        if not Prayer.query.filter_by(member_id = member.id).first():
+        # Check if the member already has a prayer request
+        existing_prayer = Prayer.query.filter_by(member_id=member.id).first()
+        if not existing_prayer:
+            # Generate a random description for the prayer request
             prayer = Prayer(
-                member_id = member.id,
-                description = random.choice(description)
+                member_id=member.id,
+                description=random.choice(description)
             )
             prayers.append(prayer)
+
     db.session.add_all(prayers)
     db.session.commit()
 
+def make_memberprayer():
+    MPInstance.query.delete()
+
+    members = Member.query.all()
+    prayers = Prayer.query.all()
+
+    concerns = []
+
+    for member in members:
+        random.shuffle(prayers)
+        for prayer in prayers[:1]:
+            # Create MPInstance only if it doesn't already exist for this member and prayer
+            existing_concern = MPInstance.query.filter_by(member_id=member.id, prayer_id=prayer.id).first()
+            if not existing_concern:
+                concern = MPInstance(
+                    member_id=member.id,
+                    prayer_id=prayer.id
+                )
+                concerns.append(concern)
+
+    db.session.add_all(concerns)
+    db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
         make_member()
         make_prayers()
+        make_memberprayer()
